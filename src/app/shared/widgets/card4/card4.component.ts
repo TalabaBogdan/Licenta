a9827d64832a5b46ee8d8731f8a2dc31799e5a43
TalabaBogdan <bogdan.talaba99@e-uvt.ro>
2021-06-30 15:35:55 +0300

    App vAproapeFinala

--- src/app/shared/widgets/card4/card4.component.ts
+++ src/app/shared/widgets/card4/card4.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import * as Highcharts from 'highcharts';
-import HC_exporting from 'highcharts/modules/exporting';
+import { ChartDataSets, ChartType } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+import { CryptoService } from 'src/app/services/crypto.service';
 
 @Component({
   selector: 'app-widget-card4',
@@ -8,81 +9,60 @@ import HC_exporting from 'highcharts/modules/exporting';
   styleUrls: ['./card4.component.scss']
 })
 export class Card4Component implements OnInit {
-
-  Highcharts = Highcharts;
-  chartOptions = {};
   
-  constructor() { }
+  constructor(private crypto: CryptoService) { }
 
-  
+  data: any = [];
+  data1: number[] = [];
+  data2: number[] = [];
+  data3: number[] = [];
+  data4: number[] = [];
+  data5: number[] = [];
+
+  barChartData: ChartDataSets[] = [];
+  barChartLabels: Label[] = [];
+
+  barChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    title: {
+        text: "Volume traded in the past 24H",
+        display: true
+    }
+  };
+
+  barChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,255,0,0.28)',
+    },
+  ];
+
+  barChartLegend = true;
+
+  barChartPlugins = [];
+
+  barChartType: ChartType = 'bar';
+
 
   ngOnInit(): void {
+    this.crypto.getObservable().subscribe((response: any) => {
+        console.log(response.data);
 
-    this.chartOptions = {
-      chart: {
-        type: 'area'
-    },
-    title: {
-        text: 'Historic and Estimated Worldwide Population Growth by Region'
-    },
-    subtitle: {
-        text: 'Source: Wikipedia.org'
-    },
-    xAxis: {
-        categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
-        tickmarkPlacement: 'on',
-        title: {
-            enabled: false
-        }
-    },
-    yAxis: {
-        title: {
-            text: 'Billions'
-        },
-    },
-    tooltip: {
-        split: true,
-        valueSuffix: ' millions'
-    },
-    plotOptions: {
-        area: {
-            stacking: 'normal',
-            lineColor: '#666666',
-            lineWidth: 1,
-            marker: {
-                lineWidth: 1,
-                lineColor: '#666666'
-            }
-        }
-    },
-    series: [{
-        name: 'Asia',
-        data: [502, 635, 809, 947, 1402, 3634, 5268]
-    }, {
-        name: 'Africa',
-        data: [106, 107, 111, 133, 221, 767, 1766]
-    }, {
-        name: 'Europe',
-        data: [163, 203, 276, 408, 547, 729, 628]
-    }, {
-        name: 'America',
-        data: [18, 31, 54, 156, 339, 818, 1201]
-    }, {
-        name: 'Oceania',
-        data: [2, 2, 2, 6, 13, 30, 46]
-    }]
-};
-HC_exporting(Highcharts);
+        this.data1.push(response.data[0].quote.USD.volume_24h);
+        this.data2.push(response.data[1].quote.USD.volume_24h);
+        this.data3.push(response.data[2].quote.USD.volume_24h);
+        this.data4.push(response.data[3].quote.USD.volume_24h);
+        this.data5.push(response.data[4].quote.USD.volume_24h);
 
-  setTimeout(()=>{
-    window.dispatchEvent(
-      new Event('resize')
-    );
-  },300)
-
+        this.barChartData = [
+          {data: this.data1, label: response.data[0].symbol},
+          {data: this.data2, label: response.data[1].symbol},
+          {data: this.data3, label: response.data[2].symbol},
+          {data: this.data4, label: response.data[3].symbol},
+          {data: this.data5, label: response.data[4].symbol}
+        ];
+        this.barChartLabels.push("Volume");
+      });
+    }
   }
-    
-
-  
-
-}
